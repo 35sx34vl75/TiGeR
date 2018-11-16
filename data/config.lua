a6a9f156ac0b9d7a6b1e491e237486059b140013
35sx34vl75 <45097836+35sx34vl75@users.noreply.github.com>
2018-11-16 15:57:59 +0330

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,30 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "Administrative",
+    "Fun",
+    "GroupManager",
+    "Msg-Checks",
+    "SetUp-Plugins",
+    "Core",
+    "Gold",
+    "Helper-Api",
+    "Help-Api",
+    "Helper-Top",
+    "test",
+    "Mutetime"
+  },
+  enabled_plugins_api = {
+    "Helper-Api",
+    "Helper-Top",
+    "Help-Api"
+  },
+  sudo_users = {
+    793857379,
+    603359850,
+    205549111
+  }
+}
+return _
+end
